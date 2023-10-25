c1264fa85ba73da6cf3c98ce620c3aaf60542c44
MABBenton <mabbenton7@gmail.com>
2023-10-24 17:29:44 -0700

    added JS animation

---
+++ script.js
@@ -0,0 +1,23 @@
+//When a user hovers over .card it triggers a vertical bouncing animation of the card elememt
+//which stops after two rounds of the animation.
+
+//create variable storing .card (card)
+const cards = document.getElementsByClassName('card');
+const cardBounce = [
+  {transform: "translate(0, 0)"},
+  {transform: "translate(0, -2rem)"},
+  {transform: "translate(0, 0)"}    
+]
+
+const cardTiming = {
+  duration: 1500,
+  iterations: 2
+}
+
+
+for (let card of cards) {
+    card.addEventListener('mouseover', () => {
+        card.animate(cardBounce, cardTiming);
+      });
+}
+
